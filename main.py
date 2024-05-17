945760eb8184d8c51bd20d853511013776b0eb8c
Haider Abbas <haidertgr677@gmail.com>
2024-05-17 10:29:34 +0530

    main.py was modified'

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import os
+import pyttsx3
+engine=pyttsx3.init()
 if __name__ == '__main__':
-    print("Welcome to RoboSpeaker 1.1. Created by Haider")
+    print("Welcome to RoboSpeaker 1.1. Created by Haider Abbas")
     while True:
         s=input("Enter what you want me to speak : ")
         if s=="Bye":

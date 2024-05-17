953d0b8e490124471c19fc5dece8109a9c0167f5
Haider Abbas <haidertgr677@gmail.com>
2024-05-17 11:12:33 +0530

    Speaker code was changed

--- main.py
+++ main.py
@@ -1,12 +1,24 @@
-import os
 import pyttsx3
-engine=pyttsx3.init()
 if __name__ == '__main__':
-    print("Welcome to RoboSpeaker 1.1. Created by Haider Abbas")
+    
+    engine=pyttsx3.init()
+    engine.say("Welcome to the TEXT TO SPEECH CONVERTER. Created by Haider Abbas " + "To close the app you can type Exit or Bye")
+    print("Welcome to the TEXT TO SPEECH CONVERTER. Created by Haider Abbas. " + "\n" + "To close the app you can type Exit or Bye")
+    engine.runAndWait()
+    
+    # RATE
+    rate=engine.getProperty('rate')
+    engine.setProperty('rate',120)
+    
+    # VOICE
+    # voices = engine.getProperty('voices')
+    # engine.setProperty('voice',voices[1].id)
+    
     while True:
-        s=input("Enter what you want me to speak : ")
-        if s=="Bye":
-           os.system("say'Have a nice day friend, Bye'")
-           break
-        command=f"say={s}"
-        os.system(command)
+        s=input("Enter what you want me to say : ")
+        if s=="BYE" or s=="bye" or s=="Bye" or s=="exit" or s=="Exit" or s=="EXIT":
+            engine.say("Thanks for using me. Have a nice day friend!")
+            engine.runAndWait()
+            break
+        engine.say(s)
+        engine.runAndWait()

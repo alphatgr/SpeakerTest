2bdfb8da80e81eb4c91abd923ab530a5e9c4130b
Haider Abbas <haidertgr677@gmail.com>
2024-06-11 21:42:35 +0530

    The program of RoboSpeaker was changed into Binary Search

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 import os
 if __name__ == '__main__':
     print("Welcome to RoboSpeaker 1.1. Created by Haider")
@@ -8,3 +9,30 @@ if __name__ == '__main__':
            break
         command=f"say={s}"
         os.system(command)
+=======
+print("Program for binary search")
+def binary_search(arr,target)
+    low=0
+    high=len(arr)-1
+    while low<=high:
+        mid = (low+high)//2
+        if arr[mid]==target:
+            return mid
+        elif low < target:
+            low = mid+1
+        else :
+            high = mid-1
+    return -1
+no_of_elements=int(input("Enter the no of elements"))
+arr=[]
+print("Enter the elements")
+for i in range (no_of_elements):
+    elements=int(input())
+    arr.append(elements)
+result=binary_search(arr,target)
+target=int(input("Enter the target element"))
+if result!=-1:
+    print ("The target element is found at the index" , result)
+else :
+    print ("The target element is not found")
+>>>>>>> Stashed changes

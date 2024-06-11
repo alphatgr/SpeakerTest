b954d297c7e29a3b81f2de6b82410d0479eff45d
Haider Abbas <haidertgr677@gmail.com>
2024-06-11 21:48:47 +0530

    Binary Search Updated

--- main.py
+++ main.py
@@ -11,15 +11,15 @@ def binary_search(arr,target):
         else :
             high = mid-1
     return -1
-no_of_elements=int(input("Enter the no of elements"))
+no_of_elements=int(input("Enter the no of elements : "))
 arr=[]
-print("Enter the elements")
+print("Enter the elements : ")
 for i in range (no_of_elements):
     elements=int(input())
     arr.append(elements)
+target=int(input("Enter the target element : "))
 result=binary_search(arr,target)
-target=int(input("Enter the target element"))
 if result!=-1:
-    print ("The target element is found at the index" , result)
+    print ("The target element is found at the index : " , result)
 else :
-    print ("The target element is not found")
+    print ("The target element is not found.")

a69b1ec10cf2682316c97c614489d65a47d420e7
Haider Abbas <haidertgr677@gmail.com>
2024-06-12 11:19:42 +0530

    The bugs were fixed

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ def binary_search(arr,target):
         mid = (low+high)//2
         if arr[mid]==target:
             return mid
-        elif low < target:
+        elif arr[low] < target:
             low = mid+1
         else :
             high = mid-1
@@ -17,7 +17,7 @@ print("Enter the elements : ")
 for i in range (no_of_elements):
     elements=int(input())
     arr.append(elements)
-target=int(input("Enter the target element : "))
+target=int(input("Enter the target element to be searched : "))
 result=binary_search(arr,target)
 if result!=-1:
     print ("The target element is found at the index : " , result)

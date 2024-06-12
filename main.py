bd73f3919e4ab00263e3a2bd7b6d188c88cc3464
Haider Abbas <haidertgr677@gmail.com>
2024-06-12 11:42:35 +0530

    The Speaker was added in the Code

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
+import pyttsx3
+engine=pyttsx3.init()
 print("Program for binary search")
+engine.say("Program for binary search")
+engine.runAndWait()
 def binary_search(arr,target):
     low=0
     high=len(arr)-1
@@ -11,15 +15,25 @@ def binary_search(arr,target):
         else :
             high = mid-1
     return -1
-no_of_elements=int(input("Enter the no of elements : "))
+engine.say("Enter the number of elements")
+engine.runAndWait()
+no_of_elements=int(input("Enter the number of elements : "))
 arr=[]
+engine.say("Enter the elements")
+engine.runAndWait()
 print("Enter the elements : ")
 for i in range (no_of_elements):
     elements=int(input())
     arr.append(elements)
+engine.say("Enter the target element to be searched")
+engine.runAndWait()
 target=int(input("Enter the target element to be searched : "))
 result=binary_search(arr,target)
 if result!=-1:
+    engine.say(f"The target element is found at the index : {result}")
+    engine.runAndWait()
     print ("The target element is found at the index : " , result)
 else :
+    engine.say("The target element is not found.")
+    engine.runAndWait()
     print ("The target element is not found.")
